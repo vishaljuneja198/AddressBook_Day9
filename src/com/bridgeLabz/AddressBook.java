abd30d078d21e3281802a8c1ed451304ce56425b
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-30 10:41:26 +0530

    Welcome to AddressBook

---
+++ src/com/bridgeLabz/AddressBook.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("WelCome to Address book");
+    }
+
+}
+
---
+++ src/com/bridgeLabz/Contacts.java
@@ -0,0 +1,4 @@
+package com.bridgeLabz;
+
+public class Contacts {
+}
