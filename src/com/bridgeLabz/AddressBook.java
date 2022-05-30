d36e1ca32dc49a063d0572d786b68183d107c2ad
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-30 10:55:00 +0530

    add contact lisit

--- src/com/bridgeLabz/AddressBook.java
+++ src/com/bridgeLabz/AddressBook.java
@@ -1,13 +1,38 @@
 package com.bridgeLabz;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("WelCome to Address book");
+    static Scanner sc = new Scanner(System.in);
+
+    ArrayList<Contacts> list = new ArrayList<>();
+
+    public void contactList() {
+        System.out.println("Enter First Name");
+        String firstName = sc.nextLine();
+
+        System.out.println("Enter last Name");
+        String lastName = sc.nextLine();
+
+        System.out.println("Enter State");
+        String state = sc.nextLine();
+
+        System.out.println("Enter City");
+        String city = sc.nextLine();
+
+        System.out.println("Enter Address");
+        String address = sc.nextLine();
+
+        System.out.println("Enter phone number");
+        String phone = sc.nextLine();
+
+        System.out.println("Enter email");
+        String email = sc.nextLine();
+
+        Contacts cList = new Contacts(firstName, lastName, address, city, state, phone, email);
+
+        list.add(cList);
+
     }
-
 }
-
