19590eff18eabc74680d73593dad46845a5eed99
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-30 11:32:47 +0530

    uc4

--- src/com/bridgeLabz/AddressBookMain.java
+++ src/com/bridgeLabz/AddressBookMain.java
@@ -1,6 +1,8 @@
 package com.bridgeLabz;
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
@@ -14,22 +16,21 @@ public class AddressBookMain {
         System.out.println("if you want to change any data of a person, reply y/n : ");
         String check = AddressBook.sc.nextLine();
 
-        System.out.println("enter name whose details you want to edit : ");
-        String name = AddressBook.sc.nextLine();
 
         if (check.equals("y")) {
+            System.out.println("enter name whose details you want to edit : ");
+            String name = AddressBook.sc.nextLine();
+
+            int i =0;
             for (Contacts cThrough : a1.list) {
 
                 if (cThrough.getFirstName().equals(name)) {
-                    System.out.println("enter updated phone number : ");
-                    cThrough.setPhone(AddressBook.sc.next());
+                    a1.list.remove(i);
                     break;
                 }
-
+                i++;
             }
         }
-
         System.out.println(a1.list);
-
     }
 }
